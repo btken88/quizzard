6ab9e8663952f4ad7875d9263c11a51243efce48
Bryce Kennedy <btken88@gmail.com>
2020-07-09 11:00:33 -0600

    Create user authorization for Games controller.

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController
-  def index
-    @games = Game.all
-    render json: { games: @games }
-  end
+  before_action :authorize, only: :create
 
   def create
-    @game = game.create({
+    @game = Game.create({
                           user_id: @user.id,
                           score: params[:score]
                         })
+    render json: { game: @game }, status: :created
+  end
+
+  private
+
+  def authorize
+    header = request.headers['Authorization']
+    token = header.split(' ')[1]
+    if !token
+      render json: { error: 'User not authorized' }, status: :forbidden
+    else
+      begin
+        secret = Rails.application.secret_key_base
+        payload = JWT.decode(token, secret)[0]
+        @user = User.find payload['user_id']
+      rescue StandardError
+        render json: { error: 'User not authorized' }, status: :forbidden unless @user
+      end
+    end
   end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  validates :username, uniqueness: true
+
+  def show
+    @user = User.find(params[:user_id])
+    render json: @user, include: :games
+  end
+
   def create
-    @user = User.create({
-                          username: params[:username],
-                          password: params[:password]
-                        })
+    @user = User.create(user_params)
 
     render json: { user: @user }, status: :created
+  end
+
+  private
+
+  def user_params
+    params.require(:username, :password)
   end
 end
